838c24de451d400d118d3f0ec9267ff94735616a
Roman Batsenko <rbatsenko@gmail.com>
2019-01-20 17:37:29 +0100

    add AppBar and Add Button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { deepPurple, pink } from '@material-ui/core/colors';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { withStyles } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: pink,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 4,
+    right: theme.spacing.unit * 4,
+  },
+});
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <MuiThemeProvider theme={theme}>
+          <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" color="inherit" noWrap>
+                Final Counter
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Fab color="secondary" aria-label="Add" className={classes.fab}>
+            <AddIcon />
+          </Fab>
+        </MuiThemeProvider>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
