b6031eeb4abae2141a11e1c962822d6d0d93204e
Roman Batsenko <rbatsenko@gmail.com>
2019-01-25 18:13:23 +0100

    add tabs

--- src/App.js
+++ src/App.js
@@ -3,53 +3,108 @@ import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { deepPurple, pink } from '@material-ui/core/colors';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+//import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import SwipeableViews from 'react-swipeable-views';
 import { withStyles } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: deepPurple,
-    secondary: pink,
-  },
-  typography: {
-    useNextVariants: true,
-  },
-});
+function TabContainer({ children, dir }) {
+  return (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
 
 const styles = theme => ({
   appBar: {
     position: 'relative',
   },
-  fab: {
-    position: 'absolute',
-    bottom: theme.spacing.unit * 4,
-    right: theme.spacing.unit * 4,
+  logo: {
+    flexGrow: 1,
   },
+  layout: {
+    width: 'auto',
+    [theme.breakpoints.up(1280 + theme.spacing.unit * 2 * 2)]: {
+      width: 1170,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  wrapper: {
+    marginBottom: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1280 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+    },
+  },
+  label: {
+    minHeight: theme.spacing.unit * 8,
+    color: 'white',
+  },
+  tabIndicator: {
+    height: 5,
+  }
 });
 
 class App extends Component {
+  state = {
+    activeTab: 0,
+  };
+
+  handleChange = (event, activeTab) => {
+    this.setState({ activeTab });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ activeTab: index });
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
 
     return (
       <React.Fragment>
         <CssBaseline />
-        <MuiThemeProvider theme={theme}>
-          <AppBar position="static" className={classes.appBar}>
-            <Toolbar>
-              <Typography variant="h6" color="inherit" noWrap>
-                Final Counter
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Fab color="secondary" aria-label="Add" className={classes.fab}>
-            <AddIcon />
-          </Fab>
-        </MuiThemeProvider>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap className={classes.logo}>
+              <span role="img" aria-label="Final Counter Logo">👑</span> Final Counter
+            </Typography>
+            <Tabs
+              value={this.state.activeTab}
+              indicatorColor="secondary"
+              textColor="secondary"
+              onChange={this.handleChange}
+              classes={{
+                indicator: classes.tabIndicator
+              }}
+            >
+              <Tab label="Bloc #1" className={classes.label} />
+              <Tab label="Bloc #2" className={classes.label} />
+              <Tab label="Bloc #3" className={classes.label} />
+              <Tab label="Bloc #4" className={classes.label} />
+              <Tab label="Results" className={classes.label} />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <main className={classes.layout}>
+          <Paper square className={classes.wrapper}>
+            <SwipeableViews
+              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+              index={this.state.activeTab}
+              onChangeIndex={this.handleChangeIndex}
+            >
+              <TabContainer dir={theme.direction}>One</TabContainer>
+              <TabContainer dir={theme.direction}>Two</TabContainer>
+              <TabContainer dir={theme.direction}>Three</TabContainer>
+              <TabContainer dir={theme.direction}>Four</TabContainer>
+              <TabContainer dir={theme.direction}>Results</TabContainer>
+            </SwipeableViews>
+          </Paper>
+        </main>
       </React.Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { pink } from '@material-ui/core/colors';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: {
+      main: '#64ffda',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+function Root() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App theme={theme} />
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(
+  <Root />
+,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
